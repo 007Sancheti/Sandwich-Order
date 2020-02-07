87bf3eb5a7b542ea20b201060a8eb00ee70d8a4d
Shubh <guptas877@gmail.com>
2020-02-07 21:32:54 +0530

    user items

--- src/hot-hoagies/user-orders.js
+++ src/hot-hoagies/user-orders.js
@@ -29,17 +29,16 @@ class UserOrders extends PolymerElement {
       <template is="dom-repeat" items={{myOrders}}>
         <paper-card image="../../images/carousal3.jpg">
         <ul>
-        <li>OrderId:{{item.orderId}}</li>
+        <li>OrderId:{{item.orderDetailId}}</li>
         <li>Price:{{item.totalPrice}}</li>
         <li>Date:{{item.orderDate}}</li>
         <li>PaymentMode:{{item.paymentMode}}</li>
         <li>Status:{{item.orderStatus}}</li>
         <li>Items:
-        <template is="dom-repeat" items={{item.orderItems}} as="order">
-        {{order.vendorItem.item.itemName}},
+        <template is="dom-repeat" items={{item.cartItems}} as="order">
+        {{order.item.itemName}},
         </template>
         </li>
-        <li>Quantity:{{item.quantity}}</li>
         </ul>
         </paper-card>
     `;
@@ -48,7 +47,7 @@ class UserOrders extends PolymerElement {
     return {
       myOrders:{
         type:Array,
-        value:[{orderId:'123', orderDate:'234'}]
+        value:[]
       },
     };
   }
@@ -66,15 +65,14 @@ class UserOrders extends PolymerElement {
   connectedCallback()
   {
     super.connectedCallback();
-    this.postObj.items= JSON.parse(sessionStorage.setItem('myCart'))
-  this.$.ajax._makeAjaxCall('get',`http://10.117.189.245:8085/hothoagies/users/${sessionStorage.getItem('userId')}/carts`,this.postObj,'myOrders')  
+  this.$.ajax._makeAjaxCall('get',`http://10.117.189.245:8085/hothoagies/users/${sessionStorage.getItem('userId')}/orders`,null,'myOrders')  
   }
   /**
    * 
    * @param {customEvent} event provide the data for dom-repeat to show the details of order
    */
-  _gettingOrders(event){  
-      this.myOrders=event.detail.data.orders
+  _gettingOrders(event){ 
+      this.myOrders=event.detail.data
   }
 }
 
